50b6bc8437019c4f9ed6193a494f765a49974312
Censera <Censera@proton.me>
2026-08-18 10:34:17 +0100

    Add guest protection event enforcement

---
+++ src/main/java/xyz/censera/guestmode/GuestProtectionListener.java
@@ -0,0 +1,76 @@
+package xyz.censera.guestmode;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+final class GuestProtectionListener implements Listener {
+    private final GuestMode plugin;
+
+    GuestProtectionListener(GuestMode plugin) {
+        this.plugin = plugin;
+    }
+
+    private boolean guest(Player player) {
+        return plugin.getRegistry().snapshot().contains(player.getUniqueId());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player player && guest(player)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onAttack(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player player && guest(player)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onTarget(EntityTargetLivingEntityEvent event) {
+        if (event.getTarget() instanceof Player player && guest(player)) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onFood(FoodLevelChangeEvent event) {
+        if (event.getEntity() instanceof Player player && guest(player)) {
+            event.setCancelled(true);
+            player.setFoodLevel(20);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onInteract(PlayerInteractEvent event) {
+        if (guest(event.getPlayer()) && event.getPlayer().getGameMode() != org.bukkit.GameMode.SPECTATOR) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onInventoryOpen(InventoryOpenEvent event) {
+        if (event.getPlayer() instanceof Player player && guest(player)
+                && player.getGameMode() != org.bukkit.GameMode.SPECTATOR) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onInventoryDrag(InventoryDragEvent event) {
+        if (event.getWhoClicked() instanceof Player player && guest(player)) {
+            event.setCancelled(true);
+        }
+    }
+}
